f630523bf228d177cb205328f2d983fd90602f56
LitMei <litmei@outlook.com>
2022-05-19 16:07:03 +0800

    添加输出文件标号；关闭自动移除文件是找不到文件时的提示，改为移除成功时提示。为生成的图片添加了序号

---
+++ 删除指定文件.py
@@ -0,0 +1,37 @@
+import os
+
+
+def del_file(path: str, deep: int = -1):
+    filename_list = ("标准差", "光滑度", "三阶中心矩", "算数平均值", "信息熵")
+    if deep > 2:
+        return False
+
+    if deep == -1:
+        deep = 0
+    else:
+        deep += 1
+
+    print(path)
+    if not os.path.exists(path):
+        return False
+    if os.path.isfile(path):
+        name = path[path.rfind("\\") + 1:path.rfind(".")]
+        if name in filename_list:
+            os.remove(path)
+            return True
+        return False
+    if os.path.isdir(path):
+        if not path[len(path) - 1] == "\\":
+            path += "\\"
+        folder = path[:path.rfind("\\") + 1]
+        for path in os.listdir(path):
+            del_file(folder + path, deep)
+    else:
+        return False
+
+if __name__ == '__main__':
+    path = input("path: ")
+    del_file(path)
+
+
+
